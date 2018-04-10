d918006dc865810928473107529644ffb081cce2
ViktorMun <v.mun@yahoo.com>
2018-04-10 18:50:08 +0200

    Order services

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import setupDb from './db'
 import {verify} from "./jwt";
 import {User} from "./users/entity";
 import LoginController from "./logins/controller";
+import  orderController  from './orders/controller'
 
 const port = process.env.PORT || 4008
 
 const app = createKoaServer({
   cors: true,
   controllers: [
-    LoginController
+    LoginController,
+    orderController
   ],
   currentUserChecker: async (action: Action) => {
     const header: string = action.request.headers.authorization
