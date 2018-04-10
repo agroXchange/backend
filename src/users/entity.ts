13c72211f2557ba4214ebb93983a1ca8c8b61f79
lucadido06 <lucadidonato06@gmail.com>
2018-04-10 12:46:06 +0200

    entities

---
+++ src/users/entity.ts
@@ -0,0 +1,30 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne } from 'typeorm'
+import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString, IsEmail, MinLength } from 'class-validator'
+import { Profile } from '../profiles/entity'
+
+
+@Entity()
+export class User extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @IsEmail()
+  @Column('text', { nullable: false })
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @Column('text', { nullable: false })
+  password: string
+
+  @IsString()
+  @Column('text', { nullable: false })
+  role: string;
+
+  @OneToOne(_ => Profile, profile => profile.user,{eager: true})
+  profile: Profile;
+
+
+ }
