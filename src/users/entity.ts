7b32157eca4cfa5d04051efb8e595977cc17e7fa
Billy <b.schrammen@gmail.com>
2018-04-12 12:16:30 +0200

    Adds adminrouts for user entity to patch user, aprove and delete user as well as get all pending users

--- src/users/entity.ts
+++ src/users/entity.ts
@@ -1,11 +1,9 @@
-import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, OneToMany, Column} from 'typeorm'
+import {Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, OneToMany} from 'typeorm'
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
 import { IsString, IsEmail, MinLength } from 'class-validator'
 import {Exclude} from 'class-transformer'
 import { Profile } from '../profiles/entity'
 import * as bcrypt from 'bcrypt'
-import {Order} from "../orders/entity";
-import {Product} from "../products/entity";
 
 @Entity()
 export class User extends BaseEntity {
@@ -32,12 +30,6 @@ export class User extends BaseEntity {
   @OneToOne(_ => Profile, {eager: true})
   @JoinColumn()
   profile: Profile;
-
-  @OneToMany(_ => Product, product => product.seller)
-  products: Product[]
-
-  @OneToMany(_ => Order, order => order.buyer)
-  orders: Order[]
 
   async setPassword(pass: string) {
     this.password = await bcrypt.hash(pass, 10)
