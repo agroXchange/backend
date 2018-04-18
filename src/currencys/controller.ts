58b2b09aac2667403ddbf6414f7da6541aea920d
Billy <b.schrammen@gmail.com>
2018-04-18 12:01:13 +0200

    Changed the interval

--- src/currencys/controller.ts
+++ src/currencys/controller.ts
@@ -1,5 +1,6 @@
 import { JsonController, Get, HttpCode, Param } from 'routing-controllers'
 import { Currency } from './entity'
+import { currencyKey } from '../mailApiKey'
 const request = require("request")
 
 
@@ -25,14 +26,13 @@ export default class currencyController {
 const minutes = 30, updateInterval = minutes * 60 * 1000
 setInterval(async function() {
   const currencys = await Currency.find()
-  const list = ['USD', 'EUR', 'CRC', 'PAB', 'COP']
   console.log("Currencys updated at: " + new Date())
   currencys.map(currency => {
-    const url = `https://data.fixer.io/api/latest?access_key=79a8743353a65dd03ada5eb0c872d0bd&base=${currency.name}&symbols=USD,EUR,CRC,PAB,COP&format=1`
+    const url = `https://data.fixer.io/api/latest?access_key=${currencyKey}&base=${currency.name}&symbols=USD,EUR,CRC,PAB,COP&format=1`
     request(url, (error, response, body) => {
       let json = JSON.parse(body)
       currency.USD = json.rates.USD
-      currency.EUR = json.rates.ERU
+      currency.EUR = json.rates.EUR
       currency.CRC = json.rates.CRC
       currency.PAB = json.rates.PAB
       currency.COP = json.rates.COP
@@ -41,5 +41,4 @@ setInterval(async function() {
     currency.updated_at = new Date()
     currency.save()
   })
-  //update currency Data
 }, updateInterval)
