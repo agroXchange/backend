168a580647d2e0911dd7792c6083be5ff66a1df7
nsch1 <nick.schooneman1@gmail.com>
2018-04-18 18:41:31 +0200

    Add socket io and make chat work

--- src/messages/controller.ts
+++ src/messages/controller.ts
@@ -1,8 +1,12 @@
-import {Body, CurrentUser, JsonController, NotFoundError, Param, Post, UnauthorizedError} from "routing-controllers";
+import {
+  Body, CurrentUser, Get, JsonController, NotFoundError, Param, Post,
+  UnauthorizedError
+} from "routing-controllers";
 import {User} from "../users/entity";
 import {Order} from "../orders/entity";
 import Message from "./entity";
 import {IsString} from 'class-validator'
+import {io} from "../index";
 
 class ValidateMessage {
   @IsString()
@@ -18,13 +22,13 @@ export default class MessageController {
     @CurrentUser() currentUser: User,
     @Param('id') id: number
   ) {
-    const order = await Order.findOne({where: {id}, relations['buyer']})
+    const order = await Order.findOne({where: {id}, relations: ['buyer']})
     if (!order) throw new NotFoundError('No order found')
 
     const {buyer, seller} = order
 
-    const senderIsBuyer = currentUser.id === buyer.id
-    const senderIsSeller = currentUser.id === seller.id
+    const senderIsBuyer = currentUser.profile.id === buyer.id
+    const senderIsSeller = currentUser.profile.id === seller.id
 
     if (!(senderIsBuyer || senderIsSeller)) {
       throw new UnauthorizedError('You are not authorized to send a message in this conversation.')
@@ -46,7 +50,33 @@ export default class MessageController {
       order
     }).save()
 
+    const action = {
+      type: 'NEW_MESSAGE',
+      payload: message
+    }
+
+    io.to(buyer.id).emit('action', action)
+    io.to(seller.id).emit('action', action)
+
     return message
+  }
+
+  @Get('/orders/:id([0-9]+)/messages')
+  async getMessages(
+    @CurrentUser() currentUser: User,
+    @Param('id') id: number
+  ) {
+    const order = await Order.findOne({where: {id}, relations: ['buyer', 'messages', 'messages.sender', 'messages.receiver']})
+    if (!order) throw new NotFoundError('No order found')
+
+    const {seller, buyer} = order
+
+    const senderIsBuyer = currentUser.profile.id === buyer.id
+    const senderIsSeller = currentUser.profile.id === seller.id
+
+    if (!(senderIsBuyer || senderIsSeller)) throw new UnauthorizedError("You're not authorized.")
+
+    return order.messages
   }
 
 }
