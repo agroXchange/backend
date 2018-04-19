217512e70bc99b063a1497cc53b64d4b7afea923
Billy <b.schrammen@gmail.com>
2018-04-19 12:42:59 +0200

    added some fields to dashboard

--- src/dashboard/controller.ts
+++ src/dashboard/controller.ts
@@ -12,22 +12,34 @@ import { Product } from '../products/entity'
 @JsonController()
 export default class dashboardController {
 
-  @Authorized()
+  // @Authorized()
   @Get('/dashboard')
   @HttpCode(200)
   async getDashboard(
-    @CurrentUser() currentUser: User
+    // @CurrentUser() currentUser: User
   ) {
+    const currentUser = await User.findOneById(4)
       if (currentUser!.role === 'admin') {
         const pendingUsers = await User.find({
           where: {approved: false}
         })
         const users = await User.find()
         const products = await Product.find()
+        const orders = await Order.find()
+        const aprovedOrders = orders.filter(order => order.status === 'Approved')
+        const penddingOrders = orders.filter(order => order.status === 'Pending')
+        const declinedOrders = orders.filter(order => order.status === 'Declined')
+        const purchasedOrders = orders.filter(order => order.status === 'Purchased')
+
         return {
           pendingUsers: pendingUsers.length,
           users: users.length,
-          products: products.length
+          products: products.length,
+          orders: orders.length,
+          approvedOrders: aprovedOrders.length,
+          declinedOders: declinedOrders.length,
+          pendingOders: penddingOrders.length,
+          purchasedOders: purchasedOrders.length
         }
       }
 
